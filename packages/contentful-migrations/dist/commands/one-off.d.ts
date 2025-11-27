fe867582c3bef4f0109c2f19c8bfafbeb592c2b4
Diogo Vaz <diogo.vaz.tavares@gmail.com>
2025-11-27 22:50:06 +0000

    chore: remove deprecated files and clean up distribution directory

--- packages/contentful-migrations/dist/commands/one-off.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Run a single one-off migration by name
- */
-export declare function oneOff(options: {
-    name: string;
-    environment?: string;
-}): Promise<void>;
-//# sourceMappingURL=one-off.d.ts.map
