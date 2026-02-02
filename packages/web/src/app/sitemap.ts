d1e53a6549bbfe5393f4ae912e15ae037387ee8f
Diogo Vaz <diogo.vaz.tavares@gmail.com>
2026-02-02 11:29:26 +0000

    fix: remove revalidade const from sitemap
    - Removed the REVALIDATION_TIME constant and related comments from the sitemap file to streamline the code.
    - Cleaned up the sitemap function by eliminating unnecessary preview mode checks, enhancing clarity and maintainability.

--- packages/web/src/app/sitemap.ts
+++ packages/web/src/app/sitemap.ts
@@ -1,20 +1,10 @@
 import type { MetadataRoute } from "next";
 
-import { REVALIDATION_TIME } from "@/lib/constants";
 import { getSitemapData } from "@/lib/features/projects/api";
 
 const BASE_URL = "https://www.alexandra-leykauf.com";
 
-export const revalidate = REVALIDATION_TIME;
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const isPreview = env.NEXT_PUBLIC_CONTENTFUL_IS_PREVIEW;
-
-  // Return empty sitemap in preview mode
-  //   if (isPreview) {
-  //     return [];
-  //   }
-
   const projects = await getSitemapData();
   const entries: MetadataRoute.Sitemap = [];
 
